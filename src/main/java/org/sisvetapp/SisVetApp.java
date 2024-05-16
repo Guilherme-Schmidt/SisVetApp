6d9d118dd15f1840a023d0c54af2db11c59673ab
Guilherme Schmidt <guischmidt201@gmail.com>
2024-05-16 13:58:08 -0300

    update: main

--- src/main/java/org/sisvetapp/SisVetApp.java
+++ src/main/java/org/sisvetapp/SisVetApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SisVetApp {
     public static void main(String[] args) {
-        System.setProperty("java.awt.headless", "false");
         SpringApplication.run(SisVetApp.class, args);
     }
 }
