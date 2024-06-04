b52a6c2043d8fdfc00f54256d3fc9cc30990f8e0
Guilherme Schmidt <guischmidt201@gmail.com>
2024-06-03 21:57:54 -0300

    add

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -13,7 +13,7 @@ import AddAnimal from "./components/Animal/AddAnimal/AddAnimal";
 import AnimalDetails from "./components/Animal/AnimalDetails/AnimalDetails";
 import EditAnimal from "./components/Animal/EditAnimal/EditAnimal";
 import AdminList from "./components/Administradores/AdminList";
-import Login from "./Login"
+import Login from "./components/auth/LoginPage";
 function App() {
   const apiURL = "http://localhost:8080";
 
@@ -30,9 +30,7 @@ function App() {
     especie: "",
     cor: "",
     prorietario: "",
-    foto: "",
-    email: "",  // Adicionando emailFunc para login
-    senha: "", 
+    foto: "",    senha: "", 
   });
 
   return (
